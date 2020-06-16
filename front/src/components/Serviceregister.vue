53e9e5798e8d0612143223dcb4d1c57e448da98b
hyerin0326 <hyehye990326@gmail.com>
2020-06-16 15:07:53 +0900

    Vue UX add (0616)

--- front/src/components/Serviceregister.vue
+++ front/src/components/Serviceregister.vue
@@ -1,144 +1,112 @@
 <template>
     <div class="row">
-        <div class="col-md-12">
-          <div class="main-title">Service Registration</div>
+        <div class="col-md-2">
+          <div class= "sub-title">All Device List</div>
         </div>
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
-           <div class="box">
-            <div class="box-text">Registration</div>
-            <div class="box-signup">
-              <label class="txt01">Device</label>
-              <input type="text" class="device-name"  placeholder="Enter Device Name">
-              <label class="txt02">Discription</label>
-              <input class="discription" type="text" placeholder="Enter Discription"/>
-              <label class="txt03">Service Name</label>
-              <input class="service-name" type="text" placeholder="Enter Service Name"/>
-              <label class="txt04">Type</label>
-              <input class="type" type="text" placeholder="Enter Type"/>
-              <label class="txt05">Tier</label>
-              <input class="tier" type="text" placeholder="Enter Tier"/>
-              <label class="txt06">API</label>
-              <input class="api" type="text" placeholder="Enter API"/>
-            </div>
-            <b-button variant="light" class="signup">R E G I S T R A T I O N</b-button></div>
-
+        <div class="col-md-1"></div>
+        <div class="col-md-9">
+          <div class="main-title">Ownership Request</div>
+        </div>
+        <div class="col-md-2">
+          <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            <b-card class="card01" title="Device name"><b-button class= "ownership">Ownership</b-button>
+               <b-card-text>
+               service-<br>2020.06.15<br>Tier
+               </b-card-text>
+            </b-card>
+            <b-card class="card02" title="Device name"><b-button class= "ownership">Ownership</b-button>
+               <b-card-text> service-<br>2020.06.15<br>Tier</b-card-text>
+            </b-card>
+            <b-card class="card03" title="Device name"><b-button class= "ownership">Ownership</b-button>
+               <b-card-text> service-<br>2020.06.15<br>Tier</b-card-text>
+            </b-card>
+             <b-card class="card04" title="Device name"><b-button class= "ownership">Ownership</b-button>
+               <b-card-text> service-<br>2020.06.15<br>Tier</b-card-text>
+            </b-card>
           </div>
-        <div class="col-md-2"></div>
+        <div class="col-md-1"></div>
+        <div class="col-md-8">
+          <b-card class="card05" title="Set Alram" sub-title="Device Info">
+             <b-button class= "request">Request</b-button>
+               <b-card-text> service-<br>2020.06.15<br>Tier</b-card-text>
+          </b-card>
+          <b-card class="card06" title="Service Info">
+            <b-card-text></b-card-text>
+          </b-card>
+        </div>
+        <div class="col-md-1"></div>
     </div>
+
 </template>
 
 <script>
 </script>
 
 <style>
+.sub-title {
+position: relative;
+top: 45px;
+font-weight: bold;
+font-size: 35px;
+}
 .main-title {
 position: relative;
 top: 30px;
 font-weight: bold;
 font-size: 50px;
 }
-.box {
-background-color: #E7E7E7;
-position: relative;
-top:90px;
-left: 190px;
-width:700px;
-height:720px;
-border-radius:50px;
+.form-control {
+  border: 1px solid #3d6098;
+  box-shadow: 0 0 0 1px #3d6098;
+  position: relative;
+  top: 50px;
+  width: 250px;
 }
-.box-text {
-font-size: 40px;
-font-weight: bold;
+.ownership {
 position: relative;
-top: 20px;
-left: 130px;
+top: 80px;
+left: 110px;
+background-color: #f04b4c;
+width:110px;
 }
-.box-signin{
-font-size: 20px;
-}
-.device-name {
-width: 400px;
-height: 45px;
+.card01 {
 position: relative;
-top: 75px;
-left: 80px;
+top:80px;
 }
-.discription {
-width: 400px;
-height: 45px;
+.card02 {
 position: relative;
-top: 115px;
-left: 130px;
+top:110px;
 }
-.service-name {
-width: 400px;
-height: 45px;
+.card03 {
 position: relative;
-top: 155px;
-left: 130px;
+top:140px;
 }
-.type {
-width: 400px;
-height: 45px;
+.card04 {
 position: relative;
-top: 195px;
-left: 130px;
+top:170px;
 }
-.tier {
-width: 400px;
-height: 45px;
+.request {
 position: relative;
-top: 230px;
-left: 130px;
+top: -60px;
+left: 950px;
+background-color: #f04b4c;
+width:100px;
 }
-.api {
-width: 400px;
-height: 45px;
+.reject {
 position: relative;
-top: 270px;
-left: 130px;
+top: 0px;
+left: 850px;
+background-color: #f04b4c;
+width:100px;
 }
-.signup {
-width: 400px;
-height:50px;
+.card05 {
 position: relative;
-top:310px;
-left:130px;
-font-size: 27px;
-font-weight: bold;
-background-color:#3d6098;
-color: white;
+top: 80px;
 }
-.txt01 {
+.card06 {
 position: relative;
-top: 30px;
-left: 130px;
-}
-.txt02 {
-position: relative;
-top:115px;
-left: -327px;
-}
-.txt03 {
-position: relative;
-top:158px;
-left: -275px;
-}
-.txt04 {
-position: relative;
-top:198px;
-left: -275px;
-}
-.txt05 {
-position: relative;
-top:235px;
-left: -275px;
-}
-.txt06 {
-position: relative;
-top:273px;
-left: -275px;
+top: 120px;
+height:450px;
 }
 
 
